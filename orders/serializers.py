0159473a9758b7f5bff3f67b747acc04a94ac48a
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-05-03 15:44:19 -0300

    feat/added view to update order status

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import Order
+from carts.models import Cart
+from products.models import Product
 
 class OrderSerializer(serializers.ModelSerializer):
+    products = serializers.SerializerMethodField()
+
     class Meta:
         model = Order
 
@@ -10,7 +14,18 @@ class OrderSerializer(serializers.ModelSerializer):
             "status",
             "created_at",
             "updated_at",
-            # "products" -> campo para listar os produtos?
+            "products"
         ]
 
-        
+    def get_user_cart_products(user)
+        cart = Cart.objects.filter(user=user).first()
+        if cart:
+            return Product.objects.filter(cart=cart)
+        else:
+            return []
+
+    def update(self, instance, validated_data):
+        instance.status = validated_data.get('status', instance.status)
+        instance.save()
+
+        return instance
--- orders/views.py
+++ orders/views.py
@@ -1,17 +1,38 @@
-from django.shortcuts import render
 from rest_framework import generics
 from rest_framework_simplejwt.authenticarion import JWTAuthentication
-from .serializers import OrderSerializer
-from .permissions import IsEmployeeAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 
-class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
+from .models import Order
+from .serializers import OrderSerializer
+from ..permissions import IsEmployee
+
+
+#Lista os produtos do pedido
+class OrderListView(generics.ListAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsEmployeeAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        return Order.objects.filter(user=user)
+
+#Atualização do status do pedido
+class OrderDetailView(generics.UpdateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsEmployee]
     
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
 
-    def perform_update(self, serializer):
+    def patch(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True))
+        serializer.is_valid(raise_exception=True)
+        serializer.save(status=request.data.get('status'))
 
-        get_object_or_404()
+        return Response(serializer.data)
+      
