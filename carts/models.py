447cefd226f7e0d1e50aeb9062bc97bc9d52341b
hermlandim <hermerson_landim@hotmail.com>
2023-05-05 11:25:00 -0300

    fix/cart-view

--- carts/models.py
+++ carts/models.py
@@ -7,6 +7,8 @@ class Cart(models.Model):
     class Meta:
         ordering = ["id"]
 
-    amount = models.IntegerField()
-    total_price = models.DecimalField(decimal_places=2, max_digits=10)
+    amount = models.IntegerField(default=0, blank=True, null=True)
+    total_price = models.DecimalField(
+        decimal_places=2, max_digits=10, default=0, blank=True, null=True
+    )
     user = models.OneToOneField("users.User", on_delete=models.CASCADE)
