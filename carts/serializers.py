1cc72efc4b3ede53184626287e9e8823993179d9
hermlandim <hermerson_landim@hotmail.com>
2023-05-05 15:05:38 -0300

    fix/cart-view

--- carts/serializers.py
+++ carts/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import Cart
 from products.models import CartProducts, Product
+from rest_framework.validators import UniqueValidator
 
 
 class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
-        fields = "__all__"
-        extra_kwargs = {"id": {"read_only": True}, "user_id ": {"read_only": True}}
+        fields = ("id", "amount", "total_price", "user_id")
+        extra_kwargs = {"id": {"read_only": True}, "user_id": {"read_only": True}}
 
 
 # Mudar para o product serializer
@@ -18,7 +19,7 @@ class ProductReturnSerializer(serializers.ModelSerializer):
 
 
 class CartReturnSerializer(serializers.ModelSerializer):
-    products_list = ProductReturnSerializer(many=True)
+    products_list = ProductReturnSerializer(many=True, read_only=True)
 
     class Meta:
         model = CartProducts
@@ -28,3 +29,6 @@ class CartReturnSerializer(serializers.ModelSerializer):
             "products_list": {"read_only": True},
         }
         # depth = 1
+
+    def create(self, validated_data):
+        return super().create(validated_data)
