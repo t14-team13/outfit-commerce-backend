45a949c87515903d0fa38a1b2d1ca20d63b76951
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-05-08 14:26:30 -0300

    feat/update develop

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import get_object_or_404
 
 from .models import Order, ProductsOrder
 from users.models import User
