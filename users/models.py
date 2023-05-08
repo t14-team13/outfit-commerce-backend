a463849ee5d8dc2d3b2486b84b4acface464a2ec
stevalves <ealves1710@hotmail.com>
2023-05-08 15:36:08 -0300

    feature/multiple_adresses

--- users/models.py
+++ users/models.py
@@ -7,11 +7,3 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     is_employee = models.BooleanField(null=True, default=False)
-
-    address = models.OneToOneField(
-        "addresses.Address",
-        on_delete=models.CASCADE,
-        related_name="address",
-        null=True,
-        default=None
-    )
